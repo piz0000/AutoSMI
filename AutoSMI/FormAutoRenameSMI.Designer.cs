c9684fa5c2c82e0dd633d1e60a73b6c9eb20fa6a
piz0000 <piz0000@naver.com>
2022-05-29 18:37:42 +0900

    Add project files.

---
+++ AutoSMI/FormAutoRenameSMI.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace PlyaerSMIAuto
+{
+    partial class FormAutoRenameSMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxVideo = new System.Windows.Forms.TextBox();
+            this.textBoxSubTitle = new System.Windows.Forms.TextBox();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.labelTag = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxVideo, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxSubTitle, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRename, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelTag, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(848, 281);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "첫번째 자막";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "첫번재 동영상";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxVideo
+            // 
+            this.textBoxVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxVideo.Location = new System.Drawing.Point(123, 4);
+            this.textBoxVideo.Name = "textBoxVideo";
+            this.textBoxVideo.ReadOnly = true;
+            this.textBoxVideo.Size = new System.Drawing.Size(722, 21);
+            this.textBoxVideo.TabIndex = 2;
+            this.textBoxVideo.Tag = "";
+            this.textBoxVideo.Click += new System.EventHandler(this.TextBox_Click);
+            // 
+            // textBoxSubTitle
+            // 
+            this.textBoxSubTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSubTitle.Location = new System.Drawing.Point(123, 34);
+            this.textBoxSubTitle.Name = "textBoxSubTitle";
+            this.textBoxSubTitle.ReadOnly = true;
+            this.textBoxSubTitle.Size = new System.Drawing.Size(722, 21);
+            this.textBoxSubTitle.TabIndex = 2;
+            this.textBoxSubTitle.Tag = "";
+            this.textBoxSubTitle.Click += new System.EventHandler(this.TextBox_Click);
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRename.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRename.Location = new System.Drawing.Point(123, 63);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(722, 44);
+            this.buttonRename.TabIndex = 3;
+            this.buttonRename.Text = "자막 이름 변경";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.ButtonRename_Click);
+            // 
+            // labelTag
+            // 
+            this.labelTag.AutoSize = true;
+            this.labelTag.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelTag.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTag.Location = new System.Drawing.Point(123, 113);
+            this.labelTag.Margin = new System.Windows.Forms.Padding(3);
+            this.labelTag.Name = "labelTag";
+            this.labelTag.Size = new System.Drawing.Size(722, 165);
+            this.labelTag.TabIndex = 4;
+            this.labelTag.Text = "@ 동영상 기준 변경\r\n@ 갯수 다르면 자막 갯수까지만 진행\r\n@ 폴더 내 동영상은 확장자를 기준으로 함\r\n@ 폴더 내 동영상 겹치지 않게 주의\r" +
+    "\n";
+            // 
+            // FormAutoRenameSMI
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(848, 281);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "FormAutoRenameSMI";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Rename SubTitle";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxVideo;
+        private System.Windows.Forms.TextBox textBoxSubTitle;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.Label labelTag;
+    }
+}
---
+++ AutoSMI/FormAutoRenameSMI.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace PlyaerSMIAuto
+{
+    public partial class FormAutoRenameSMI : Form
+    {
+        public FormAutoRenameSMI()
+        {
+            InitializeComponent();
+        }
+
+        void TextBox_Click(object sender, EventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Multiselect = false;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    tb.Text = ofd.FileName;
+                }
+            }
+        }
+
+        void ButtonRename_Click(object sender, EventArgs e)
+        {
+            string strVideo = textBoxVideo.Text;
+            string strSubTitle = textBoxSubTitle.Text;
+
+            if (string.IsNullOrWhiteSpace(strVideo) || string.IsNullOrWhiteSpace(strSubTitle))
+            {
+                return;
+            }
+            if (strVideo == strSubTitle)
+            {
+                return;
+            }
+
+            string[] playList = GetFile(strVideo);
+            string[] subList = GetFile(strSubTitle);
+            if (playList == null || playList.Length <= 0 || subList == null || subList.Length <= 0)
+            {
+                return;
+            }
+
+            if (playList.Length != subList.Length)
+            {
+                if (MessageBox.Show("파일 수 다름 실행?", "확인", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            RunRename(playList, subList);
+        }
+
+        /// <summary>
+        /// 동영상 파일 이름으로 자막 파일 이름 변경 <para></para>
+        /// 1:1 순서대로 진행
+        /// </summary>
+        /// <param name="playList"></param>
+        /// <param name="subList"></param>
+        void RunRename(string[] playList, string[] subList)
+        {
+            string folder = Path.GetDirectoryName(subList[0]);
+
+            string subExtension = Path.GetExtension(subList[0]);
+
+            int maxCount = playList.Length > subList.Length ? subList.Length : playList.Length;
+
+            for (int index = 0; index < maxCount; index++)
+            {
+                //동영상 파일 명 확장자 없이 가져오기
+                string playName = Path.GetFileNameWithoutExtension(playList[index]);
+
+                try
+                {
+                    File.Move(subList[index], folder + "\\" + playName + subExtension);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 탐색기 정렬과 동일하게 가져오기
+        /// </summary>
+        /// <param name="fullName"></param>
+        /// <returns></returns>
+        string[] GetFile(string fullName)
+        {
+            string extension = Path.GetExtension(fullName);
+            string[] list = Directory.GetFiles(Path.GetDirectoryName(fullName), "*" + extension, SearchOption.TopDirectoryOnly);
+            if (list != null && list.Length >= 2)
+            {
+                Array.Sort(list, new MyComparer());
+            }
+            return list;
+        }
+
+        public class MyComparer : IComparer<string>
+        {
+            [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+            static extern int StrCmpLogicalW(String x, String y);
+
+            public int Compare(string x, string y)
+            {
+                return StrCmpLogicalW(x, y);
+            }
+        }
+
+
+
+
+
+    }
+}
